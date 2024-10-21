27269c9d48cd077fc30243541182d5e415fb099c
حور <74899552+hr12-abd@users.noreply.github.com>
2024-10-21 21:34:53 +0300

    init

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/node_modules/bootstrap/dist/css/bootstrap.rtl.min.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<main>
+    <div class="container">
+        
+    </div>
+</main>
+
+<script src="assets/css/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
